605f343c3ab0d79c42ada99ed67ac1e1a3d2fb5d
sherlock6147 <tooshort9541@gmail.com>
2020-05-24 07:26:43 +0530

    get game player data from user

--- headerfiles/play.hpp
+++ headerfiles/play.hpp
@@ -7,4 +7,36 @@ struct player
 {
     string role="NULL";
     string name="NULL";
-}player1,player2;
+    int score=0;
+};
+class game
+{
+    private:
+        player player1,player2;
+        string question,answer,hint;
+        char userGuess;
+        int qMaster,ans;
+    public:
+        game()
+        {
+            getgamedata();
+        }
+        void getgamedata()
+        {
+            cout << "Please enter player 1's name: ";
+            getline ( cin , player1.name );
+            cout << "Please enter player 2's name: ";
+            getline ( cin , player2.name );
+            cout << "Who would like to be QUESTION MASTER first?(PLAYER NUMBER) ";
+            cin>>qMaster;
+            if (qMaster==1 || qMaster==2)
+            {
+                ans=(qMaster==1)?2:1;
+            }
+            else
+            {
+                cout<<"Invalid arguement\n";
+                getgamedata();
+            }
+        }
+};
