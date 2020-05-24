9b2ee7fadc79917c8661f94bcd14f3f49c0aa6fe
sherlock6147 <tooshort9541@gmail.com>
2020-05-24 07:11:41 +0530

    added player structure definition

---
+++ headerfiles/play.hpp
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <string.h>
+#include "displayHangman.hpp"
+using namespace std;
+string qMaster = "QUESTION MASTER" , Player = "PLAYER";
+struct player
+{
+    string role="NULL";
+    string name="NULL";
+}player1,player2;
